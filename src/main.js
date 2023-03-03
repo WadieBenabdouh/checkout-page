b7920690d4682e2e79fcc7e21f5d6452810ac972
Wadie Benabdouh <wadie.trade@gmail.com>
2023-03-04 00:31:03 +0100

    first JS success 2023

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
-let count = 0;
+// FIRST ITEM
+let count = 1;
 
-document.getElementById("decreaseBtn").onclick = function () {
+document.getElementById("decreaseBtn1").onclick = function () {
   count -= 1;
   document.getElementById("countoneLabel").innerHTML = count;
 };
 
-document.getElementById("resetBtn").onclick = function () {
-  count = 0;
-  document.getElementById("countoneLabel").innerHTML = count;
-};
-
-document.getElementById("increaseBtn").onclick = function () {
+document.getElementById("increaseBtn1").onclick = function () {
   count += 1;
   document.getElementById("countoneLabel").innerHTML = count;
+};
+// SECOND ITEM
+let countTwo = 1;
+
+document.getElementById("decreaseBtn2").onclick = function () {
+  countTwo -= 1;
+  document.getElementById("counttwoLabel").innerHTML = countTwo;
+};
+
+document.getElementById("increaseBtn2").onclick = function () {
+  countTwo += 1;
+  document.getElementById("counttwoLabel").innerHTML = countTwo;
 };
